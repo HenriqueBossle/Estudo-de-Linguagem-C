c3de44e7089aa858dbf725ee28a3d088a83b5f0c
Henrique Bossle <henriquegoncalvesbossle@gmail.com>
2024-08-09 14:52:36 -0300

    Codigos adicionados
    09/08/24

---
+++ Projetos C/testedebug.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+j	printf("Mensagem1.\n");
+	printf("Mensagem2.\n");
+	printf("Mensagem3.\n");
+	printf("Mensagem4.\n");
+	printf("Mensagem5.\n");
+	
+	return 0;
+}
